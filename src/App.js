d0f90d9cb632db35c7b733db24617ca2b1250dff
AndressaStefany <andressa.stefany@yahoo.com.br>
2018-04-05 12:35:58 -0300

    adaptation for version react-router-dom 4.2.0

--- src/App.js
+++ src/App.js
@@ -1,69 +1,77 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
 import RaisedButton from 'material-ui/RaisedButton'
+import Home from './components/Home'
+import About from './components/About'
 
 class App extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       open: false
     }
   }
 
-  toggleDrawer = () => this.setState({ open: !this.state.open })
+  toggleDrawer = () => this.setState({ open: !this.state.open });
 
   render() {
     return (
-      <div>
-        <AppBar
-          title="Title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onLeftIconButtonTouchTap={this.toggleDrawer}
-        />
+        <Router>
+          <div>
+            <AppBar
+                title="Title"
+                iconClassNameRight="muidocs-icon-navigation-expand-more"
+                onLeftIconButtonClick={this.toggleDrawer}
+            />
 
-        <Drawer
-          docked={false}
-          width={300}
-          onRequestChange={this.toggleDrawer}
-          open={this.state.open}
-        >
-          <AppBar title="Title" onLeftIconButtonTouchTap={this.toggleDrawer} />
-          <MenuItem
-            primaryText="home"
-            containerElement={<Link to="/" />}
-            onTouchTap={() => {
-              console.log('going home')
-              alert('going home!')
-              this.toggleDrawer()
-            }}
-          />
-          <MenuItem
-            primaryText="about"
-            containerElement={<Link to="/about" />}
-            onTouchTap={() => {
-              console.log('about')
-              alert('going to about page!')
-              this.toggleDrawer()
-            }}
-          />
-        </Drawer>
+            <Drawer
+                docked={false}
+                width={300}
+                onRequestChange={this.toggleDrawer}
+                open={this.state.open}
+            >
+              <AppBar title="Title" onLeftIconButtonTouchTap={this.toggleDrawer} />
+              <MenuItem
+                  primaryText="home"
+                  containerElement={<Link to="/" />}
+                  onTouchTap={() => {
+                    console.log('going home')
+                    alert('going home!')
+                    this.toggleDrawer()
+                  }}
+              />
+              <MenuItem
+                  primaryText="about"
+                  containerElement={<Link to="/about" />}
+                  onTouchTap={() => {
+                    console.log('about')
+                    alert('going to about page!')
+                    this.toggleDrawer()
+                  }}
+              />
+            </Drawer>
+            <div style={{ textAlign: 'center' }}>
+              {this.props.children}
 
-        <div style={{ textAlign: 'center' }}>
-          {this.props.children}
+              <RaisedButton
+                  label="Toggle Drawer"
+                  onTouchTap={this.toggleDrawer}
+                  style={{marginTop: '3%'}}
+              />
 
-          <RaisedButton
-            label="Toggle Drawer"
-            onTouchTap={this.toggleDrawer}
-          />
-        </div>
+              <Route exact path='/' component={Home}/>
+              <Route path='/about' component={About}/>
+            </div>
 
-      </div>
+
+          </div>
+        </Router>
     )
   }
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { HashRouter } from 'react-router-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import App from './App'
-import Home from './components/Home'
-import About from './components/About'
 import './index.css'
 
-injectTapEventPlugin()
+injectTapEventPlugin();
 
 const Root = () => (
-  <MuiThemeProvider>
-    <Router history={hashHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Home} />
-        <Route path="about" component={About} />
-      </Route>
-    </Router>
-  </MuiThemeProvider>
-)
+    <MuiThemeProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </MuiThemeProvider>
+);
 
 ReactDOM.render(
-  <Root />,
-  document.getElementById('root')
-)
+    <Root />,
+    document.getElementById('root')
+);
